fa423c97219532d9be2463e45e3ede6f9d1083b6
Milindsm <milu.mangale@gmail.com>
2023-04-30 17:16:04 +0530

    added header component with cart button

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Fragment } from 'react';
+import './App.css';
+import Header from './component/Layout/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
