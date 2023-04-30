348540eb80751e37cb1f2129fcf9252ef934b52c
Milindsm <milu.mangale@gmail.com>
2023-04-30 17:44:36 +0530

    added mealSummary component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import { Fragment } from 'react';
 import './App.css';
 import Header from './component/Layout/Header';
+import MealsSummary from './component/Meals/MealsSummary';
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <MealsSummary/>
     </Fragment>
   );
 }
---
+++ src/component/Meals/MealsSummary.js
@@ -0,0 +1,11 @@
+import classes from './MealsSummary.module.css'
+const MealsSummary = () =>{
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious food, Delivered to you</h2>
+            <p>Choose your Favorite food from our board selection of available meals</p>
+        </section>
+    )
+}
+
+export default MealsSummary;
